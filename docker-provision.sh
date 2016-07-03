85bcf3a3b34b84da68bb90773b8dd1b0970da87e
Paulo Sousa <paulo.sousa@corp.globo.com>
2016-07-03 17:28:47 -0300

    remove deprecated docker option

--- docker-provision.sh
+++ docker-provision.sh
@@ -2,5 +2,5 @@
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 echo "deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -c | awk '{print $2}') main" | tee /etc/apt/sources.list.d/docker.list
 apt-get update && apt-get install linux-image-extra-$(uname -r) -y --force-yes && apt-get install docker-engine -y --force-yes
-sed -i -e '/^DOCKER_OPTS/d' /etc/default/docker && echo "DOCKER_OPTS=' -e native -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock'" | tee -a /etc/default/docker
+sed -i -e '/^DOCKER_OPTS/d' /etc/default/docker && echo "DOCKER_OPTS='-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock'" | tee -a /etc/default/docker
 ( stop docker && start docker ) || start docker
